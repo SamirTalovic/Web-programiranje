f5667ec82f9409b4ecdb942f6a55145745536187
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2022-11-10 21:22:25 +0100

    Php form

---
+++ phppriprema/display.php
@@ -0,0 +1,15 @@
+<?php
+echo $_POST['name'];
+echo $_POST['adress'];
+echo $_POST['city'];
+echo $_POST['state'];
+echo $_POST['zipcode'];
+echo $_POST['customer'];
+echo $_POST['typeof'];
+echo $_POST['size'];
+echo $_POST['extras'][0];
+echo $_POST['extras'][1];
+echo $_POST['extras'][2];
+echo $_POST['extras'][3];
+echo $_POST['comment'];
+?>
